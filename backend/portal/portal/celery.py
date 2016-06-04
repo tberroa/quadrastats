6f6ec53fd46a10230bef7b66915be4e50616f413
tberroa <tberroa@outlook.com>
2016-06-04 22:55:16 +0000

    small celery config fix

--- backend/portal/portal/celery.py
+++ backend/portal/portal/celery.py
@@ -4,11 +4,11 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal.settings')
 
 from django.conf import settings
 
-app = Celery('proj')
+app = Celery('portal')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
