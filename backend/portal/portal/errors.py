56f1bfc8a7b8502c85feafbd4bee51ba7bb34200
tberroa <tberroa@outlook.com>
2016-06-08 22:12:51 -0400

    updated summoners, basically complete

--- backend/portal/portal/errors.py
+++ backend/portal/portal/errors.py
@@ -1,16 +1,22 @@
-# invalid request format = 10
-# summoner already registered = 15
-# incorrect summoner/password combination = 20
+# friend already listed = 5
+# internal processing error = 10
+# invalid credentials = 15
+# invalid request format = 20
 # invalid response from riot servers = 25
-# internal processing error = 30
+# summoner already registered = 30
+# summoner does not exist = 35
 
-invalid_request_format = {"error": 10, \
+friend_already_listed = {"error" : "5", \
+                         "message" : "friend already listed"}
+internal_processing_error = {"error" : 10, \
+                             "message" : "internal processing error"}
+invalid_credentials = {"error" : 15, \
+                       "message" : "invalid credentials"}
+invalid_request_format = {"error" : 20, \
                           "message" : "invalid request format"}
-summoner_already_registered = {"error": 15, \
-                               "message" : "summoner already registered"}
-incorrect_credentials = {"error": 20, \
-                         "message" : "incorrect summoner/password combination"}
 invalid_riot_response = {"error": 25, \
                          "message" : "invalid response from riot servers"}
-internal_processing_error = {"error": 30, \
-                             "message" : "internal processing error"}
+summoner_already_registered = {"error" : 30, \
+                               "message" : "summoner already registered"}
+summoner_does_not_exist = {"error" : 35, \
+                           "message" : "summoner does not exist"}
--- backend/portal/summoners/views.py
+++ backend/portal/summoners/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import hashers
 from django.core import serializers
 from django.shortcuts import render
+from portal.errors import friend_already_listed
 from portal.errors import internal_processing_error
+from portal.errors import invalid_credentials
 from portal.errors import invalid_request_format
 from portal.errors import invalid_riot_response
 from portal.errors import summoner_already_registered
+from portal.errors import summoner_does_not_exist
 from portal.riotapi import get_summoner
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,6 +16,103 @@ from .models import Summoner
 from .models import User
 from .serializers import SummonerSerializer
 from .serializers import UserSerializer
+
+class AddFriend(APIView):
+    def post(Self, request, format=None):
+        # extract data
+        data = request.data
+        region = data.get("region")
+        summoner_key = data.get("summoner_key")
+        friend_key = data.get("friend_key")
+
+        # validate
+        if None in (region, summoner_key, friend_key):
+            return Response(invalid_request_format)
+
+        summoner_key = format_key(summoner_key)
+        friend_key = format_key(friend_key)
+
+        # get the users summoner object
+        try:
+            summoner = Summoner.objects.get(region = region, key = summoner_key)
+        except Summoner.DoesNotExist:
+            return Response(summoner_does_not_exist)
+
+        # check if friend is already listed
+        if summoner.friends is not None:
+            friends = summoner.friends.split(",")
+            for friend in friends:
+                if friend == friend_key:
+                    return Response(friend_already_listed)
+
+        # ensure a summoner object exists for the friend
+        try:
+            Summoner.objects.get(region = region, key = friend_key)
+        except Summoner.DoesNotExist:
+            val = get_summoner(region, friend_key)
+            if val[0] != 200:
+                return Response(invalid_riot_response)
+            else:
+                friend = val[1]
+            Summoner.objects.create(region = region, \
+                                    key = friend_key, \
+                                    name = friend.get("name"), \
+                                    summoner_id = friend.get("id"), \
+                                    profile_icon = friend.get("profileIconId"))
+
+        # add the friends key to the users friend list
+        if summoner.friends is not None:
+            summoner.friends += friend_key + ","
+        else:
+            summoner.friends = friend_key + ","
+        summoner.save()
+
+        # return the users updated summoner object
+        return Response(SummonerSerializer(summoner).data)
+
+class GetSummoners(APIView):
+    def post(self, request, format=None):
+        # extract data
+        data = request.data
+        region = data.get("region")
+        keys = data.get("keys")
+
+        # validate
+        if None in (region, keys):
+            return Response(invalid_request_format)
+
+        # get all requested summoner objects
+        summoners = []
+        for key in keys:
+            try:
+                summoners.append(Summoner.objects.get(region = region, key = format_key(key)))
+            except Summoner.DoesNotExist:
+                return Response(summoner_does_not_exist)
+
+        # return the summoner objects
+        return Response(SummonerSerializer(summoners, many=True).data)
+
+class LoginUser(APIView):
+    def post(self, request, format=None):
+        # extract data
+        data = request.data
+        region = data.get("region")
+        key = data.get("key")
+        password = data.get("password")
+
+        # validate
+        if None in (region, key, password):
+            return Response(invalid_request_format)
+
+        key = format_key(key)
+
+        try:
+            summoner = Summoner.objects.get(region = region, key = key)
+            if hashers.check_password(password, summoner.user.password):
+                return Response(SummonerSerializer(summoner).data)
+            return Response(invalid_credentials)
+        except Summoner.DoesNotExist:
+            return Response(invalid_credentials)
 
 class RegisterUser(APIView):
     def post(self, request, format = None):
@@ -68,95 +168,8 @@ class RegisterUser(APIView):
             return Response(serializer.data)
         return Response(internal_processing_error)
 
-class LoginUser(APIView):
-    def post(self, request, format=None):
-        # extract data
-        data = request.data
-        region = data.get("region")
-        key = data.get("key")
-        password = data.get("password")
-
-        # validate
-        if None in (region, key, password):
-            return Response("error")
-
-        key = format_key(key)
-
-        try:
-            summoner = Summoner.objects.get(region = region, key = key)
-            if hashers.check_password(password, summoner.user.password):
-                return Response(SummonerSerializer(summoner).data)
-            return Response("incorrect combination")
-        except Summoner.DoesNotExist:
-            return Response("incorrect combination")
-
-class GetSummoners(APIView):
-    def post(self, request, format=None):
-        # set data and initialize array for storing summoner objects
-        data = request.data
-        summoners = []
-
-        # get all requested summoner objects
-        for entry in data:
-            region = entry.get("region")
-            key = format_key(entry.get("key"))
-            summoners.append(Summoner.objects.get(region = region, key = key))
-        
-        # return the summoner objects
-        return Response(SummonerSerializer(summoners, many=True).data)
-
-class AddFriend(APIView):
-    def post(Self, request, format=None):
-        # extract data
-        data = request.data
-        region = data.get("region")
-        summoner_key = data.get("summoner_key")
-        friend_key = data.get("friend_key")
-
-        # validate
-        if None in (region, summoner_key, friend_key):
-            return Response("error")
-
-        summoner_key = format_key(summoner_key)
-        friend_key = format_key(friend_key)
-
-        # get the users summoner object
-        summoner = Summoner.objects.get(region = region, key = summoner_key) 
-
-        # ensure a summoner object exists for the friend
-        ensure_summoner_exists(region, friend_key)
-
-        # add the friends key to the users friend list
-        if summoner.friends is not None:
-            summoner.friends += friend_key + ","
-        else:
-            summoner.friends = friend_key + ","
-        summoner.save()
-
-        # return the users updated summoner object
-        return Response(SummonerSerializer(summoner).data)
-
-# used to ensure a summoner exists in the database
-def ensure_summoner_exists(region, key):
-    try:
-        return Summoner.objects.get(region = region, key = key)
-    except Summoner.DoesNotExist:
-        pass
-
-    # get the summoners information via riot
-    summoner = get_summoner(region, key)[1]
-
-    # use gathered info to create summoner in database
-    summoner = Summoner.objects.create(region = region, \
-                                       key = key, \
-                                       name = summoner.get("name"), \
-                                       profile_icon = summoner.get("profile_icon"))
-
-    # returns a summoner model object
-    return summoner
-
+# remove white spaces and make all lowercase
 def format_key(key):
-    # remove white spaces and make all lowercase
     return(key.replace(" ", "").lower())
     
 
