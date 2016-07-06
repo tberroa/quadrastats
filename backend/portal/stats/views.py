f51e884d088649c051f91ddc42ea1d26aa42ebe9
tberroa <tberroa@outlook.com>
2016-07-06 15:22:57 -0400

    backend update

--- backend/portal/stats/views.py
+++ backend/portal/stats/views.py
@@ -22,13 +22,18 @@ class GetMatchStats(APIView):
         if keys is None:
             return Response(invalid_request_format)
 
-        # gather the stats for each summoner, store in list
+        # initialize list for storing the requested match stats
         stats = []
+
+        # iterate over the list of keys
         for key in keys:
+            # get summoner object
             try:
                 summoner = Summoner.objects.get(key = format_key(key))
             except Summoner.DoesNotExist:
                 return Response(summoner_does_not_exist)
+
+            # construct the query based on given parameters
             query = MatchStats.objects.filter(summoner_id = summoner.summoner_id).order_by("-match_creation")
             if champion is not None and champion != 0:
                 query = query.filter(champion = champion)
@@ -36,8 +41,11 @@ class GetMatchStats(APIView):
                 query = query.filter(lane = lane)
             if role is not None:
                 query = query.filter(role = role)
+
+            # execute query, only store the 20 most recent entries
             stats += query[:20]
 
+        # return the list of match stats
         return Response(MatchStatsSerializer(stats, many=True).data)
 
 class GetSeasonStats(APIView):
@@ -45,7 +53,9 @@ class GetSeasonStats(APIView):
         data = request.data
         return Response(data)
 
-class GetChampionStats(APIView):
+class GetSeasonStatsChampion(APIView):
     def post(self, request, format=None):
         data = request.data
         return Response(data)
+
+
