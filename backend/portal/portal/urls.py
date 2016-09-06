9e485dc874357920f755e0c7c5052701679531b1
tberroa <tberroa@outlook.com>
2016-09-06 03:24:30 -0400

    backend update

--- backend/portal/portal/urls.py
+++ backend/portal/portal/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
+
+
+def loaderio(request):
+    return HttpResponse('loaderio-89fb758787fb03c2be250691d3565029', content_type='text/plain')
 
 urlpatterns = [
+    url(r'^loaderio-89fb758787fb03c2be250691d3565029/', loaderio),
     url(r'^summoners/', include('summoners.urls')),
     url(r'^stats/', include('stats.urls')),
     url(r'^admin/', admin.site.urls),
