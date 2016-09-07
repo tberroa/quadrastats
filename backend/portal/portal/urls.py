64ab9654754421461f9dcfd15b927f502ecd91f6
tberroa <tberroa@outlook.com>
2016-09-07 00:09:25 -0400

    backend fix

--- backend/portal/portal/urls.py
+++ backend/portal/portal/urls.py
@@ -3,15 +3,25 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from portal.keys import SUPER_USER_PASSWORD
 
+# create the super user account
+try:
+    User.objects.get(username='tberroa')
+except User.DoesNotExist:
+    User.objects.create_superuser(username='tberroa', email='tberroa@outlook.com', password=SUPER_USER_PASSWORD)
 
+# setup verification file for loaderio
 def loaderio(request):
     return HttpResponse('loaderio-89fb758787fb03c2be250691d3565029', content_type='text/plain')
 
 urlpatterns = [
-    url(r'^loaderio-89fb758787fb03c2be250691d3565029/', loaderio),
     url(r'^summoners/', include('summoners.urls')),
     url(r'^stats/', include('stats.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^loaderio-89fb758787fb03c2be250691d3565029/', loaderio),
 ]
+
+
