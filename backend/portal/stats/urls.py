654b4dd5a003404b15f39edfd19d9c17570073a6
tberroa <tberroa@outlook.com>
2016-09-13 08:43:26 -0400

    backend reverted

--- backend/portal/stats/urls.py
+++ backend/portal/stats/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.urlpatterns import format_suffix_patterns
 from stats import views
 
 urlpatterns = [
-    url(r'^match/$', csrf_exempt(views.GetMatchStats.as_view())),
-    url(r'^season/$', csrf_exempt(views.GetSeasonStats.as_view())),
+    url(r'^match/$', views.GetMatchStats.as_view()),
+    url(r'^season/$', views.GetSeasonStats.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/portal/summoners/urls.py
+++ backend/portal/summoners/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import url
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.urlpatterns import format_suffix_patterns
 from summoners import views
 
 urlpatterns = [
-    url(r'^add-friend/$', csrf_exempt(views.AddFriend.as_view())),
-    url(r'^change-email/$', csrf_exempt(views.ChangeEmail.as_view())),
-    url(r'^change-password/$', csrf_exempt(views.ChangePassword.as_view())),
-    url(r'^get/$', csrf_exempt(views.GetSummoners.as_view())),
-    url(r'^login/$', csrf_exempt(views.LoginUser.as_view())),
-    url(r'^register/$', csrf_exempt(views.RegisterUser.as_view())),
-    url(r'^remove-friend/$', csrf_exempt(views.RemoveFriend.as_view())),
-    url(r'^reset-password/$', csrf_exempt(views.ResetPassword.as_view())),
-    url(r'^test1/$', csrf_exempt(views.Test1.as_view())),
+    url(r'^add-friend/$', views.AddFriend.as_view()),
+    url(r'^change-email/$', views.ChangeEmail.as_view()),
+    url(r'^change-password/$', views.ChangePassword.as_view()),
+    url(r'^get/$', views.GetSummoners.as_view()),
+    url(r'^login/$', views.LoginUser.as_view()),
+    url(r'^register/$', views.RegisterUser.as_view()),
+    url(r'^remove-friend/$', views.RemoveFriend.as_view()),
+    url(r'^reset-password/$', views.ResetPassword.as_view()),
+    url(r'^test1/$', views.Test1.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
