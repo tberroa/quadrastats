06405cf260ebbed7ee8ee756cd33a0c370ff5c05
tberroa <tberroa@outlook.com>
2016-09-14 01:21:40 -0400

    backend update

--- backend/portal/portal/riot.py
+++ backend/portal/portal/riot.py
@@ -10,7 +10,7 @@ from summoners.models import Summoner
 QUEUE = "TEAM_BUILDER_DRAFT_RANKED_5x5"
 SEASON = "SEASON2016"
 baseriotapi.set_api_key(RIOT_API_KEY)
-baseriotapi.set_rate_limits((600, 10), (36000, 600))
+baseriotapi.set_rate_limits((300, 10), (18000, 600))
 
 
 def format_key(key):
