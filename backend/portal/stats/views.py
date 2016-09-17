dccc99fd1e8f8cda76982b011be0b5645d2dea68
tberroa <tberroa@outlook.com>
2016-09-16 21:12:41 -0400

    added caching to getmatch/getseason

--- backend/portal/stats/views.py
+++ backend/portal/stats/views.py
@@ -66,7 +66,7 @@ def get_match_stats(request):
         # enter stats into cache by key
         for key in keys_extra:
             stats_key = [x for x in stats_extra if x.summoner_key == key]
-            cache.set(region + key + "match", stats_key)
+            cache.set(region + key + "match", stats_key, None)
 
     # return the stats
     return HttpResponse(stats_serializer(stats))
@@ -88,18 +88,48 @@ def get_season_stats(request):
     if None in (region, keys):
         return HttpResponse(json.dumps(INVALID_REQUEST_FORMAT))
 
-    # turn list of keys into list of Q objects
-    queries = [Q(region=region, summoner_key=format_key(key)) for key in keys]
+    # initialize list of stats to be returned
+    stats = []
 
-    # take one Q object from the list
-    query = queries.pop()
+    # initialize list of keys which were not found in the cache
+    keys_extra = []
 
-    # or the Q object with the ones remaining in the list
-    for item in queries:
-        query |= item
+    # look through the cache first
+    for key in keys:
+        # ensure proper key format
+        key = format_key(key)
 
-    # query the database
-    stats = list(SeasonStats.objects.filter(query))
+        # request stats from cache
+        result = cache.get(region + key + "season")
+
+        # evaluate result of cache request
+        if result is not None:
+            stats += result
+        else:
+            keys_extra.append(key)
+
+    # look through the database if required
+    if keys_extra:
+        # turn list of extra keys into list of Q objects
+        queries = [Q(region=region, summoner_key=key) for key in keys_extra]
+
+        # take one Q object from the list
+        query = queries.pop()
+
+        # or the Q object with the ones remaining in the list
+        for item in queries:
+            query |= item
+
+        # query the database
+        stats_extra = list(SeasonStats.objects.filter(query))
+
+        # add the stats to the list which will be returned
+        stats += stats_extra
+
+        # enter stats into cache by key
+        for key in keys_extra:
+            stats_key = [x for x in stats_extra if x.summoner_key == key]
+            cache.set(region + key + "season", stats_key, None)
 
     # return the stats
     return HttpResponse(stats_serializer(stats))
