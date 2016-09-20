2f78909691310c68b388b933497740554f085f4c
tberroa <tberroa@outlook.com>
2016-09-20 10:49:57 -0400

    more fixes

--- backend/worker/worker/wsgi.py
+++ backend/worker/worker/wsgi.py
@@ -3,5 +3,5 @@ import pymysql
 from django.core.wsgi import get_wsgi_application
 
 pymysql.install_as_MySQLdb()
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "worker.settings")
 application = get_wsgi_application()
