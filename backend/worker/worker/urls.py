08dbaec57e9f17f1dc86c6d9646b1b7118d150aa
tberroa <tberroa@outlook.com>
2016-09-20 10:55:42 -0400

    more fixeS

--- backend/worker/worker/urls.py
+++ backend/worker/worker/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
-from portal.riot import update
+from worker.riot import update
 
 urlpatterns = [
     url(r'^update', csrf_exempt(update)),
